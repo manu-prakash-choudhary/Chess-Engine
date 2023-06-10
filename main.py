1587eca86acf93c34c597ac4d711d63419e59677
Hemantsharma20 <112706384+Hemantsharma20@users.noreply.github.com>
2023-06-10 11:00:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-#     print()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
